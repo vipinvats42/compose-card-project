ff5ae1f0a6a42b0a6ff5822c9f961821c92fe82b
Vipin <vignesh.raju@daimler.com>
2024-07-13 22:26:41 +0530

    credit card demp app compose

--- app/src/main/java/com/test/creditcardapp/MainActivity.kt
+++ app/src/main/java/com/test/creditcardapp/MainActivity.kt
@@ -3,13 +3,21 @@ package com.test.creditcardapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.test.creditcardapp.designcomposables.CreditCard
+import com.test.creditcardapp.model.ModelCardInfo
 import com.test.creditcardapp.ui.theme.CreditCardAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +27,42 @@ class MainActivity : ComponentActivity() {
             CreditCardAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                   
+                   DisplayCards()
                 }
             }
         }
+    }
+}
+@Composable
+fun DisplayCards(){
+val cards = listOf(
+    ModelCardInfo("1111 2222 3333 4444",
+        "Pranshi",
+        R.drawable.mastercard,
+        R.drawable.cardback1),
+    ModelCardInfo("6666 7777 8888 4444",
+        "Pranshi tyagi",
+        R.drawable.mastercard,
+        R.drawable.cardback1),
+    ModelCardInfo("9999 6666 2222 1111",
+        "Pranshee",
+        R.drawable.mastercard,
+        R.drawable.cardback1),
+    ModelCardInfo("0000 3333 5555 8888",
+        "Pranshi ji",
+        R.drawable.mastercard,
+        R.drawable.cardback4)
+)
+
+
+    LazyColumn(modifier = Modifier
+        .fillMaxHeight()
+        .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)) {
+
+        items(cards) {
+           card-> CreditCard(cardInfo = card)
+       }
     }
 }
 
--- app/src/main/java/com/test/creditcardapp/designcomposables/CreditCard.kt
+++ app/src/main/java/com/test/creditcardapp/designcomposables/CreditCard.kt
@@ -1,30 +1,88 @@
 package com.test.creditcardapp.designcomposables
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.test.creditcardapp.R
 import com.test.creditcardapp.model.ModelCardInfo
 
 @Composable
-fun CreditCard(cardInfo : ModelCardInfo){
+fun CreditCard(cardInfo: ModelCardInfo){
 
-    Card(modifier = Modifier.height(200.dp),
+    Card(
+        modifier = Modifier.height(200.dp),
         shape = RoundedCornerShape(8.dp),
-        elevation = CardDefaults.cardElevation(10.dp)
-    ) {
-         Image(painter = painterResource(id = cardInfo.backgroundImage)
-             , contentDescription = "Visa ",
-             modifier = Modifier.fillMaxSize(),
-             contentScale = ContentScale.FillBounds
-             )
+        elevation = CardDefaults.cardElevation(8.dp)
+    ){
+        Box() {
+
+            Image(
+                painter = painterResource(id = cardInfo.backgroundImage),
+                contentDescription = "Card background",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.FillBounds
+            )
+
+
+            Box(modifier = Modifier.padding(16.dp).fillMaxSize()) {
+                Image(
+                    painter = painterResource(id = cardInfo.providerImage),
+                    contentDescription = "Provider Image",
+                    modifier = Modifier
+                        .width(86.dp)
+                        .align(Alignment.TopStart)
+                )
+
+                Column(modifier = Modifier.align(Alignment.BottomStart)) {
+
+                    Text(
+                        text = cardInfo.cardNumber,
+                        fontSize = 16.sp,
+                        letterSpacing = 1.2.sp
+                    )
+                    Text(
+                        text = cardInfo.cardHolder,
+                        fontSize = 16.sp,
+                        letterSpacing = 1.1.sp,
+
+                        )
+
+                }
+
+
+            }
+        }
+
+
+
     }
+
+}
+
+@Composable
+@Preview(showBackground = true)
+fun CreditCardPreview(){
+    val cardInfoDemo = ModelCardInfo("1234 5678 9087 1235",
+        "Pranshee",
+        R.drawable.mastercard,
+        R.drawable.cardback1)
+
+
+    CreditCard(cardInfoDemo)
 }
